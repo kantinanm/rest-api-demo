57376ce8db0d69a8d5af246fcd4204396f6490cf
basicgts <basicgts@gmail.com>
2021-04-15 12:43:50 +0700

    Completed CRUD Operation for Student

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -70,6 +70,130 @@ const addStudent = async (req, res, next) => {
   }
 };
 
+const updateStudent = async (req, res, next) => {
+  try {
+    console.log(`updateStudent `);
+    const { studentId } = req.params;
+    const studentResult = await pool.query(
+      'UPDATE student SET ?  WHERE  ?',
+      [req.body, studentId],
+      (err) => {
+        if (err) throw err;
+      },
+    );
+
+    if (
+      studentResult &&
+      Array.isArray(studentResult) &&
+      studentResult[0] &&
+      studentResult[0].affectedRows > 0
+    ) {
+      res.send({
+        result: [
+          {
+            affectedRows: studentResult[0].affectedRows,
+          },
+        ],
+        message: 'updated successfuly',
+      });
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+const deleteStudent = async (req, res, next) => {
+  try {
+    console.log(`deleteStudent `);
+    const { studentId } = req.params;
+    const studentResult = await pool.query(
+      'DELETE from student WHERE  ?',
+      [studentId],
+      (err) => {
+        if (err) throw err;
+      },
+    );
+
+    if (
+      studentResult &&
+      Array.isArray(studentResult) &&
+      studentResult[0] &&
+      studentResult[0].affectedRows > 0
+    ) {
+      res.send({
+        result: [
+          {
+            affectedRows: studentResult[0].affectedRows,
+          },
+        ],
+        message: 'delete successfuly',
+      });
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+const getStudents = async (req, res, next) => {
+  /*
+   let page = req.query.page;
+   let limit = req.query.limit;
+  req.query.*/
+  console.log(`getStudents `);
+  const { page, limit } = req.query;
+  try {
+    let sqlQuery = `SELECT * FROM student `;
+    const result = await pool.query(sqlQuery);
+    if (result[0].length < 1) {
+      throw new Error('All Students was not found');
+    }
+    res.send({
+      result: [
+        {
+          output: result[0],
+        },
+      ],
+      total_record: result[0].length,
+      message: `Result OK. `,
+    });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+const getStudentById = async (req, res, next) => {
+  /*
+  req.params.student_id
+  */
+  console.log(`getStudentById `);
+  const { studentId } = req.params;
+  try {
+    let sqlQuery = `SELECT * FROM student `;
+    if (req.params.student_id != null) {
+      sqlQuery = `WHERE student_id ='${studentId}'`;
+    }
+    const result = await pool.query(sqlQuery);
+    if (result[0].length < 1) {
+      throw new Error('All Students was not found');
+    }
+    res.send({
+      result: [
+        {
+          output: result[0],
+        },
+      ],
+      total_record: result[0].length,
+      message: `Result OK. `,
+    });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
 module.exports = {
   addStudent,
+  getStudents,
+  getStudentById,
+  updateStudent,
+  deleteStudent,
 };
--- routes/student_route.js
+++ routes/student_route.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const { addStudent } = require('../controllers/studentController');
+const {
+  addStudent,
+  getStudents,
+  getStudentById,
+  updateStudent,
+  deleteStudent,
+} = require('../controllers/studentController');
 
 const router = express.Router();
 
 router.post('/student', addStudent);
-
+router.get('/student', getStudents);
+router.get('/student/:studentId', getStudentById);
+router.put('/student/:studentId', updateStudent);
+router.delete('/student/:studentId', deleteStudent);
 module.exports = {
   routes: router,
 };
